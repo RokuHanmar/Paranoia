61b919b8a4a306e67487ed14b6e7693eab79e274
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-11-30 08:09:12 +0000

    Update Dice.c
    Added seed to prevent same random sequence being outputted whenever code is run
    
    Code now counts successes and outputs them

--- Dice.c
+++ Dice.c
@@ -6,6 +6,8 @@
 int main() {
 
     int successes = 0;
+    time_t t6;
+    srand ((unsigned) time (&t6));
 
     // Find Difficulty Class
     printf("%s", "Enter Difficulty Class: ");
@@ -20,11 +22,22 @@ int main() {
     // Roll number of dice and count successes
     for (int i = 0; i < NoDe; i++) {
         int die = rand() % 6;
+        while (die == 0) {
+            die = rand() % 6;
+        }
         printf("%d", die);
         if (die >= 5) {
             successes++;
         }
     }
+    printf("\n");
+
+    if (successes == 1) {
+        printf("%s\n", "You have 1 success");
+    } else {
+        printf("%s %d %s \n", "You have", successes, "successes");
+    }
+    
 
     if (successes >= DC) {
         printf("%s\n", "Roll succeeded");
